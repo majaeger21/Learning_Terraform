be15fc8bf4589a8bbf67d109e3c4006cba490ddf
Meyli Jaeger <64625098+majaeger21@users.noreply.github.com>
2025-04-29 13:35:39 -0700

    fixed errors

--- main.tf
+++ main.tf
@@ -31,16 +31,16 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0
+  version = "4.13.0"
   name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules       = ["all-all"]
-  engress_cidr_blocks = ["0.0.0.0/0]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
